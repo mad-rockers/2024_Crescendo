1b389325cf82b98020339898e695bd72ae95a30e
TankTheMcFrank <frankhherring@gmail.com>
2024-04-04 11:54:27 -0500

    updated delay to 1.0 seconds

--- src/main/java/frc/robot/commands/LowerIntake.java
+++ src/main/java/frc/robot/commands/LowerIntake.java
@@ -31,7 +31,7 @@ public class LowerIntake extends Command {
         && m_ShooterSubsystem.getRightRecieverSwitch()) {
       m_Timer.start();
       m_isTimerStarted = true;
-    } else if (m_isTimerStarted && m_Timer.get() > 0.5) {
+    } else if (m_isTimerStarted && m_Timer.get() > 1.0) {
       m_ShooterSubsystem.stopAllMotors("front_shooter_motor");
       m_isFinished = true;
     }
